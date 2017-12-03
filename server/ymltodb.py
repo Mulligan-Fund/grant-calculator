9d6c88129fe436df3e700650226b7764c20f0c9c
Andrew Lovett-Barron <andrew@relaystudio.com>
2017-12-03 18:45:31 -0500

    Script to extract db fields from yaml and create mongoose schema

---
+++ server/ymltodb.py
@@ -0,0 +1,37 @@
+import yaml,pprint
+
+def generateSchema(l):
+	target = ""
+
+	lines = []
+	lines.append("var mongoose = require('mongoose');\n")
+	lines.append("var Schema = mongoose.Schema;\n")
+	lines.append("var grantmakerSchema = new Schema({\n")
+
+	for i in l:
+		lines.append(i["field"]+":"+i["type"]+",\n")
+
+	lines.append("});")
+	# target.write(lines)
+
+	fo = open("schema.js", "w")
+	fo.writelines( lines )
+	fo.close()
+
+lo = []
+
+with open("../_data/grantseeker.yml", 'r') as stream:
+    out = yaml.load(stream)
+
+    for sect in out[0]["sections"]:
+    	for quest in sect["questions"]:
+    		o = {"field": quest['dbfield'], "type": quest['type']}
+    		lo.append(o)
+    		print o
+	    	# print quest['dbfield']
+
+generateSchema(lo)
+
+
+
+
